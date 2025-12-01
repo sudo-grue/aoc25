96455d0465d02bc67c250d444fd1c5b20ea91f7a
Kyle Matuszczak <kyle.l.matuszczak@gmail.com>
2025-12-01 14:04:08 -0500

    d01 polished

--- d01/p1.py
+++ d01/p1.py
@@ -1,4 +1,29 @@
-mini = """
+"""
+The attached document (your puzzle input) contains a sequence of rotations, one
+per line, which tell you how to open the safe. A rotation starts with an L or R
+which indicates whether the rotation should be to the left (toward lower numbers)
+or to the right (toward higher numbers). Then, the rotation has a distance value
+which indicates how many clicks the dial should be rotated in that direction.
+
+So, if the dial were pointing at 11, a rotation of R8 would cause the dial to
+point at 19. After that, a rotation of L19 would cause it to point at 0.
+
+Because the dial is a circle, turning the dial left from 0 one click makes it
+point at 99. Similarly, turning the dial right from 99 one click makes it
+point at 0.
+
+So, if the dial were pointing at 5, a rotation of L10 would cause it to
+point at 95. After that, a rotation of R5 could cause it to point at 0.
+
+The dial starts by pointing at 50.
+
+You could follow the instructions, but your recent required official North Pole
+secret entrance security training seminar taught you that the safe is actually a
+decoy. The actual password is the number of times the dial is left pointing at
+0 after any rotation in the sequence.
+
+For example, suppose the attached document contained the following rotations:
+
 L68
 L30
 R48
@@ -9,6 +34,26 @@ L1
 L99
 R14
 L82
+
+Following these rotations would cause the dial to move as follows:
+
+    The dial starts by pointing at 50.
+    The dial is rotated L68 to point at 82.
+    The dial is rotated L30 to point at 52.
+    The dial is rotated R48 to point at 0.
+    The dial is rotated L5 to point at 95.
+    The dial is rotated R60 to point at 55.
+    The dial is rotated L55 to point at 0.
+    The dial is rotated L1 to point at 99.
+    The dial is rotated L99 to point at 0.
+    The dial is rotated R14 to point at 14.
+    The dial is rotated L82 to point at 32.
+
+Because the dial points at 0 a total of three times during this process,
+the password in this example is 3.
+
+Analyze the rotations in your attached document. What's the actual
+password to open the door?
 """
 
 
@@ -16,25 +61,20 @@ def main():
     answer = 0
     ptr = 50
 
-    f = open("input.txt", "r", encoding="utf-8")
+    with open("input.txt", "r", encoding="utf-8") as f:
+        for line in f:
+            line = line.strip()
 
-    # for line in mini.split():
-    for line in f.readlines():
-        line = line.strip()
-        move = line[0]
-        amount = int(line[1:])
+            match line:
+                case turn if turn.startswith("L"):
+                    ptr -= int(turn[1:])
 
-        if move == "L":
-            ptr -= amount
-        else:
-            ptr += amount
+                case turn if turn.startswith("R"):
+                    ptr += int(turn[1:])
 
-        ptr %= 100
-
-        if 0 == ptr:
-            answer += 1
-
-    f.close()
+            ptr %= 100
+            if 0 == ptr:
+                answer += 1
 
     print(answer)
 
--- d01/p2.py
+++ d01/p2.py
@@ -1,14 +1,32 @@
-mini = """
-L68
-L30
-R48
-L5
-R60
-L55
-L1
-L99
-R14
-L82
+"""
+You remember from the training seminar that "method 0x434C49434B" means you're
+actually supposed to count the number of times any click causes the dial to
+point at 0, regardless of whether it happens during a rotation or
+at the end of one.
+
+Following the same rotations as in the above example, the dial points at zero
+a few extra times during its rotations:
+
+    The dial starts by pointing at 50.
+    The dial is rotated L68 to point at 82; during this rotation, it points at 0 once.
+    The dial is rotated L30 to point at 52.
+    The dial is rotated R48 to point at 0.
+    The dial is rotated L5 to point at 95.
+    The dial is rotated R60 to point at 55; during this rotation, it points at 0 once.
+    The dial is rotated L55 to point at 0.
+    The dial is rotated L1 to point at 99.
+    The dial is rotated L99 to point at 0.
+    The dial is rotated R14 to point at 14.
+    The dial is rotated L82 to point at 32; during this rotation, it points at 0 once.
+
+In this example, the dial points at 0 three times at the end of a rotation,
+plus three more times during a rotation. So, in this example,
+the new password would be 6.
+
+Be careful: if the dial were pointing at 50, a single rotation like R1000
+would cause the dial to point at 0 ten times before returning back to 50!
+
+Using password method 0x434C49434B, what is the password to open the door?
 """
 
 
@@ -16,25 +34,25 @@ def main():
     answer = 0
     ptr = 50
 
-    f = open("input.txt", "r", encoding="utf-8")
+    with open("input.txt", "r", encoding="utf-8") as f:
+        for line in f:
+            line = line.strip()
 
-    # for line in mini.split():
-    for line in f.readlines():
-        line = line.strip()
-        move = line[0]
-        amount = int(line[1:])
+            direction = line[0]
+            amount = int(line[1:])
 
-        for _ in range(amount):
-            if move == "L":
-                ptr -= 1
-            else:
-                ptr += 1
+            cycles, remainder = divmod(amount, 100)
+            answer += cycles
 
-            ptr %= 100
-            if 0 == ptr:
+            if direction == "L":
+                remainder *= -1
+
+            result = ptr + remainder
+
+            if (0 != ptr and ((result <= 0) or (100 <= result))):
                 answer += 1
 
-    f.close()
+            ptr = result % 100
 
     print(answer)
 
